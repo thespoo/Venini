007d1d38275ae1e16140d14686d585e761c0b1a4
Paul Silvester <p.s.silvester@gmail.com>
2013-07-11 15:46:42 +0200

    jquery working for links at bottom, about to make it so they only scroll when opening the links.

--- js/custom.js
+++ js/custom.js
@@ -4,19 +4,20 @@ $(document).ready(function() {
 		$('.footer-contact').slideUp();
 		$('.footer-follow').slideUp();
 		$('.footer-find').slideToggle("slow");
+		$('html,body').animate({scrollTop: $('.footer-find').offset().top},'slow');
 	});
 	
 	$('.contact').click(function() {
 		$('.footer-find').slideUp();
 		$('.footer-follow').slideUp();
 		$('.footer-contact').slideToggle("slow");
-			$('body').scrollTop(0);
-
+		$('html,body').animate({scrollTop: $('.footer-contact').offset().top},'slow');
 	});
 	
 	$('.follow').click(function() {
 		$('.footer-find').slideUp();
 		$('.footer-contact').slideUp();
 		$('.footer-follow').slideToggle("slow");
+		$('html,body').animate({scrollTop: $('.footer-follow').offset().top},'slow');
 	});
 });
